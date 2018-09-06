a7b3d60f49495ac75a5764b9322b58c0ff5febf2
TahaKJ <36728687+TahaKJ@users.noreply.github.com>
2018-09-06 10:31:01 +0200

    Update main.go

--- hello-gie/main.go
+++ hello-gie/main.go
@@ -45,7 +45,13 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, world!\n")
+	p, err := loadPage(title)
+	fmt.Fprintf(w, "Hello, GIE!\n")
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+        "<form action=\"/save/%s\" method=\"POST\">"+
+        "<textarea name=\"body\">%s</textarea><br>"+
+        "<input type=\"submit\" value=\"Save\">"+
+        "</form>", p.Title, p.Title, p.Body)
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
